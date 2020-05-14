c7e082eb011cc0ff46dde16d8790bf0b64fa570b
Sławomir Gnyś <49480725+sgnys@users.noreply.github.com>
2020-05-14 15:22:44 +0200

    Updates

---
+++ precache-manifest.6210a5dad3aed621e9fd6f3353febd75.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6bf75f6cc48b3acf4ab",
+    "url": "/react-pogoda/static/css/main.4343ecae.chunk.css"
+  },
+  {
+    "revision": "c6bf75f6cc48b3acf4ab",
+    "url": "/react-pogoda/static/js/main.b4637575.chunk.js"
+  },
+  {
+    "revision": "2f10cb7b09131f178e84",
+    "url": "/react-pogoda/static/js/runtime~main.05892948.js"
+  },
+  {
+    "revision": "d612bcf0cabf6195ffdc",
+    "url": "/react-pogoda/static/js/2.8d16de4c.chunk.js"
+  },
+  {
+    "revision": "ea44b3f60a8a15fbb1dd46641e09785e",
+    "url": "/react-pogoda/static/media/showerRain.ea44b3f6.png"
+  },
+  {
+    "revision": "594489b92414a5a2c4968ab580524da5",
+    "url": "/react-pogoda/static/media/fewClouds.594489b9.png"
+  },
+  {
+    "revision": "da9a69a371cf0601fa6e3d9e1b5b61b0",
+    "url": "/react-pogoda/static/media/rain.da9a69a3.png"
+  },
+  {
+    "revision": "b2713366eec94a59d089aa4db9a77204",
+    "url": "/react-pogoda/static/media/scatteredClouds.b2713366.png"
+  },
+  {
+    "revision": "0c6db61b6f3a4f8b1b67de78e19a9470",
+    "url": "/react-pogoda/static/media/snow.0c6db61b.png"
+  },
+  {
+    "revision": "f680ed925d2c8e323492cbbf21a94854",
+    "url": "/react-pogoda/static/media/thunderstorm.f680ed92.png"
+  },
+  {
+    "revision": "22dc64eae9250606f066621c348b7084",
+    "url": "/react-pogoda/static/media/brokenClouds.22dc64ea.png"
+  },
+  {
+    "revision": "7b45a68d9de8e74c7bb08a3fb91f37ff",
+    "url": "/react-pogoda/static/media/clearSky.7b45a68d.png"
+  },
+  {
+    "revision": "c6e6714219444825a9792704113bd92e",
+    "url": "/react-pogoda/static/media/mist.c6e67142.png"
+  },
+  {
+    "revision": "e13487409534caffaa834f43b021d2d7",
+    "url": "/react-pogoda/static/media/clearSkyNight.e1348740.png"
+  },
+  {
+    "revision": "612dc383db223cf6f645758ebfbca28a",
+    "url": "/react-pogoda/static/media/thunderstormNight.612dc383.png"
+  },
+  {
+    "revision": "a40bc25e096750b2f3743023784dde73",
+    "url": "/react-pogoda/static/media/fewCloudsNight.a40bc25e.png"
+  },
+  {
+    "revision": "aba42085d64a9567b8248b8d107bd3e1",
+    "url": "/react-pogoda/static/media/rainNight.aba42085.png"
+  },
+  {
+    "revision": "8dbe25c765aac31fa63e037ca54db8dc",
+    "url": "/react-pogoda/static/media/scatteredCloudsNight.8dbe25c7.png"
+  },
+  {
+    "revision": "a3c0d51554eabee1b778a79a6e08225f",
+    "url": "/react-pogoda/static/media/showerRainNight.a3c0d515.png"
+  },
+  {
+    "revision": "44f3387c2ee5feaddfdd7c46937ba105",
+    "url": "/react-pogoda/static/media/snowNight.44f3387c.png"
+  },
+  {
+    "revision": "22dc64eae9250606f066621c348b7084",
+    "url": "/react-pogoda/static/media/brokenCloudsNight.22dc64ea.png"
+  },
+  {
+    "revision": "c6e6714219444825a9792704113bd92e",
+    "url": "/react-pogoda/static/media/mistNight.c6e67142.png"
+  },
+  {
+    "revision": "272ecf3389efd42ccdb9ded19046c897",
+    "url": "/react-pogoda/static/media/nature.272ecf33.jpg"
+  },
+  {
+    "revision": "b152608be87cf18555e02b0db452daa5",
+    "url": "/react-pogoda/index.html"
+  }
+];
